3e4e2b07f15bdd3619050852c6f514676e9f5fc9
Hadaly <hadalyvf@gmail.com>
2022-06-24 20:19:58 +0200

    booking

--- models/Bookings.php
+++ models/Bookings.php
@@ -34,28 +34,7 @@
         }
 
         //Insert 
-        public function InsertHome(){ 
- 
-                $sql = "INSERT INTO Homes(Name, City, Price) VALUES (:name, :city, :price);";
-                $stmt= $this->connection->prepare($sql);
-                $stmt->execute(array(":name"=>$this->name, ":city"=>$this->city, ":price"=>$this->price)); 
-                return true;
-              
-        }
- 
-        public function EditHome(){    
-            $sql = "UPDATE Homes SET Name = :name, City = :city, Price = :price WHERE Id =:id";
-            $stmt= $this->connection->prepare($sql);
-            $stmt->execute(array(":id"=>$this->id, ":name"=>$this->name, ":city"=>$this->city, ":price"=>$this->price)); 
-        }
-
-
-        public function DeleteHome($id){
-            echo $id;
-            $sql = "DELETE FROM Homes WHERE Id = :id";
-            $stmt= $this->connection->prepare($sql);
-            $stmt->execute(array(":id"=>$id));
-        }
+   
 
         
     }

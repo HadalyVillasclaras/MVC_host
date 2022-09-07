dc48fb1eb42dc2f95a03a1f88634155dd7ad4261
Hadaly <hadalyvf@gmail.com>
2022-09-07 21:55:41 +0200

    indexController

--- Controller/IndexController.php
+++ Controller/IndexController.php
@@ -1,29 +1,18 @@
 <?php
 
+class IndexController extends Controller
+{      
+    private $table = 'Homes';
+    private $homeModel;
 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-    class IndexController extends Controller
-    {      
-        public function __construct()
-        { 
-        }
-        private $table = 'Homes';
-
-        public function index()
-        { 
-
-            var_dump($_SESSION);
-            $this->view('Index/main');
-            $this->model('Home'); 
-
-            $home = new Home();
-
-            $homes = $home->getAll($this->table);  
-            $this->view('Home/homes', $homes); 
-        }
+    public function __construct()
+    {
+        $this->homeModel = $this->model('Home');
     }
-
-
-?>
+    
+    public function index()
+    {
+        $allHomes = $this->homeModel->getAll($this->table);  
+        $this->view('Index/Home', $allHomes);
+    }
+}
--- View/Index/Home.php
+++ View/Index/Home.php
@@ -44,3 +44,21 @@
 <section class="hm-pg-landscape bg-nopadd" >
     <img src="<?=BASE_URL?>assets/img/bg-landscp.png" alt="">
 </section>
+
+<section class="home-sect" >
+    <h3>Our destinations</h3>
+    <article class="homes">
+        <?php foreach($data as $home): ?>
+            <div class="home-thumb">
+                <a href="<?= BASE_URL . 'homecontroller/homesinglepage?id=' . $home['id']?>">
+                    <img src="<?=BASE_URL . 'assets/img/' . $home['image_folder'] . '/' . $home['image_name'];?>">
+                    <div class="thumbnl-info">
+                        <h5 class="home-thb-title"><?=$home['name']?></h5> 
+                        <p class="home-thb-price" ><?=$home['price'] . '€ night'?></p> 
+                    </div>
+                    
+                </a>
+            </div> 
+        <?php endforeach; ?> 
+    </article>
+</section>

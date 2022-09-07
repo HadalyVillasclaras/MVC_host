6762544e892e721d44d28f11db4b0a91b7d80f6e
Hadaly <hadalyvf@gmail.com>
2022-09-07 21:39:25 +0200

    router class

--- libraries/Router.php
+++ libraries/Router.php
@@ -1,47 +1,46 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
 
-class Core 
+class Router 
 {
     protected $currentController = 'IndexController';
     protected $currentMethod = 'index';
     protected $params = []; 
 
-    public function __construct(){    
-
+    public function __construct()
+    {    
         $url = $this->getUrl();
 
-        // Class controller
-        if(isset($url)){
-            if(file_exists('../controller/' . ucwords($url[0]) . '.php')){
+        // Class
+        if (isset($url)) {
+            if (file_exists('../controller/' . ucwords($url[0]) . '.php')) {
                 $this->currentController = ucwords($url[0]);
+            } else {
+                echo 'Class does not exist.';
             }
         }
-        
-        require_once '../controller/' . $this->currentController . '.php';
+
+        require_once '../Controller/' . $this->currentController . '.php';
         $this->currentController = new $this->currentController;
-        
-        // Method class
+
+        // Method
         if(isset($url[1])){
             if(method_exists($this->currentController, $url[1])){
                 $this->currentMethod = $url[1];
                 unset($url[1]);
-            } 
+            } else {
+                echo 'Method does not exists.';
+            }
         }
 
         // Params
         $this->params = $url ? array_values($url) : [];
 
+
         call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
-    
-    
-    
-    
     }
 
-    public function getUrl(){
+    public function getUrl()
+    {
         if(isset($_GET['url'])){
             $url = rtrim($_GET['url'], '/');
             $url = filter_var($url, FILTER_SANITIZE_URL);
--- public/index.php
+++ public/index.php
@@ -1,5 +1,4 @@
 <?php 
- 
 
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
@@ -7,11 +6,11 @@ error_reporting(E_ALL);
 
 require_once '../config/parameters.php'; 
 require_once '../controller/Controller.php';
-require_once '../libraries/Core.php';
+require_once '../libraries/Router.php';
 require_once '../libraries/session.php';
 require_once '../view/head.php';
 require_once '../view/navbar.php';
 
-$init = new Core();
+$init = new Router();
 
 require_once '../view/footer.php';

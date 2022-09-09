d163d1ced8ca0dcb52313538dd7c894f87df9bcc
Hadaly Villasclaras <hadalyvf@gmail.com>
2022-09-09 11:09:46 +0200

    Update checkout()

--- Controller/ReservationController.php
+++ Controller/ReservationController.php
@@ -82,53 +82,40 @@ class ReservationsController extends Controller{
     public function checkOut(){
         $isLoggedIn = new Session();
 
-    if (!$isLoggedIn->isLoggedIn()) {
-        header("Location: " . BASE_URL . 'usercontroller/login');
-    }
+        if (!$isLoggedIn->isLoggedIn()) {
+            header("Location: " . BASE_URL . 'usercontroller/login');
+        }
 
-    if (isset($_GET['id'])) {
-        $this->homeModel->id = $_GET['id'];
-        $home = $this->homeModel->getSingleRow();  
-        
-        $data = [
-            'UserId' => $_SESSION['user_id'],
-            'HomeId' => $_GET['id'],
-            'Name' => $home['Name'],
-            'City' => $home['City'],
-            'Price' => $home['Price'],
-            'ImageName' => $home['ImageName'],
-            'ImageFolder' => $home['ImageFolder'],
-            'startDate' => $_GET['checkin'],
-            'endDate' => $_GET['checkout'],
-            'Guests' => $_GET['guests'],
-            'Nights' => '',
-            'totalCost' => ''
-        ];
-    }
+        if (isset($_GET['id'])) {
+            $this->homeModel->id = $_GET['id'];
+            $home = $this->homeModel->getSingleRow();  
+
+            $data = [
+                'UserId' => $_SESSION['user_id'],
+                'HomeId' => $_GET['id'],
+                'Name' => $home['Name'],
+                'City' => $home['City'],
+                'Price' => $home['Price'],
+                'ImageName' => $home['ImageName'],
+                'ImageFolder' => $home['ImageFolder'],
+                'startDate' => $_GET['checkin'],
+                'endDate' => $_GET['checkout'],
+                'Guests' => $_GET['guests']
+            ];
+
+            //require Reservation Class,
+            $reservation = new Reservation($data);
+
+            $data = [
+                'Nights' =>  $reservation->calculateTotalNights(),
+                'totalCost' => $reservation->calculateCost()
+            ]
+
+            //check if errors and:
+            $this->submitReservation($data);
+        }
     
-
-    $data = [
-        'UserId' => $_SESSION['user_id'],
-        'HomeId' => $_GET['id'],
-        'Name' => $home['Name'],
-        'City' => $home['City'],
-        'Price' => $home['Price'],
-        'ImageName' => $home['ImageName'],
-        'ImageFolder' => $home['ImageFolder'],
-
-        'startDate' => $_GET['checkin'],
-        'endDate' => $_GET['checkout'],
-        'Guests' => $_GET['guests'],
-        'Nights' => '',
-        'totalCost' => ''
-    ];
-
-    $data = $this->calculateCost($data); 
-    $this->submitReservation($data);
-
-
-    $this->view('Reservation/checkout', $data);   
-
+        $this->view('Reservation/checkout', $data);   
     }
 
     public function submitReservation($data){

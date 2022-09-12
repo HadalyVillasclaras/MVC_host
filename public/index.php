cc09d240f85b41408fd17e7e16b221f83b49c0c8
Hadaly <hadalyvf@gmail.com>
2022-09-12 16:46:42 +0200

    env

--- public/index.php
+++ public/index.php
@@ -8,9 +8,10 @@ require_once '../config/parameters.php';
 require_once '../controller/Controller.php';
 require_once '../lib/Router.php';
 require_once '../lib/session.php';
+
 require_once '../view/head.php';
 require_once '../view/navbar.php';
 
-$init = new Router();
+new Router();
 
 require_once '../view/footer.php';

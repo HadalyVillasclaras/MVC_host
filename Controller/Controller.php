15b447952f5cc4f2ebe5027d522184e741ecee7b
Hadaly Villasclaras <hadalyvf@gmail.com>
2022-09-12 07:27:17 +0200

    Update Controller.php

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -8,16 +8,16 @@ class Controller
             require_once '../Model/' . $model . '.php';
             return new $model();
         } else {
-            die("Model does not exists.");
+            throw new Exception('Model does not exist.");
         }
     }
 
     public function view($view, $data = [], $errors = [])
     { 
-        if(file_exists('../View/' .  $view . '.php')){  
+        if (file_exists('../View/' .  $view . '.php')) {  
             require_once '../View/' . $view . '.php';
-        }else{
-            die("View does not exists.");
+        } else {
+            throw new Exception('View does not exist.");
         }
     }
 }
